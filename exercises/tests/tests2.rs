c75c31dee36c93af7213806eef1aad7cfad27a50
Sarvesh Patkar <psarvesh314@gmail.com>
2022-09-13 00:04:46 -0400

    Completed till iterators

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -3,12 +3,11 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let s = String::from("987");
+        assert_eq!(s, "987");
     }
 }
